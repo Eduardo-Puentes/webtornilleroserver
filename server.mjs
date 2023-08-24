dea67fdff13e3ff6ca9a18b2d61c7132b105d09b
eduardopuentescs <eduardo@codestacksystems.com>
2023-08-24 08:58:15 -0600

    first commit

---
+++ server.mjs
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import "./loadEnvironment.mjs";
+import records from "./routes/record.mjs";
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+
+app.use(cors({
+  origin: ['http://localhost:3000'],
+  methods: ['POST', 'GET', 'PUT', 'DELETE'],
+  credentials: true
+}));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+  res.header("Access-Control-Allow-Headers", "Content-Type")
+}) 
+
+app.use("/record", records);
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
