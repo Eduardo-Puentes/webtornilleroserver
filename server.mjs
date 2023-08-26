016b4e35acdfc16ecdc8cb4a86b7c76993110216
eduardopuentescs <eduardo@codestacksystems.com>
2023-08-25 21:06:58 -0600

    n

--- server.mjs
+++ server.mjs
@@ -6,24 +6,8 @@ import records from "./routes/record.mjs";
 const PORT = 5050;
 const app = express();
 
-const corsOptions ={
-  origin:'*', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200,
-}
-
-app.use(cors({
-  origin: ['http://localhost:3000'],
-  methods: ['POST', 'GET', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-  res.header("Access-Control-Allow-Headers", "Content-Type")
-}) 
 
 app.use("/record", records);
 
